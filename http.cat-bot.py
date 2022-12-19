fd3901b2230581f960d00e6cca64585d0dc79baa
Chopper1337 <josephmg@tutanota.com>
2022-12-19 00:54:42 +0000

    Initial commit

---
+++ http.cat-bot.py
@@ -0,0 +1,54 @@
+import discord
+import re
+from dotenv import load_dotenv
+
+load_dotenv()
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+response_codes = [100, 101, 102, 200, 201, 202, 203, 204, 205, 206, 207, 208, 226, 300, 301, 302, 303, 304, 305, 306, 307, 308, 400, 401, 402, 403, 404, 405, 406,
+                  407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 421, 422, 423, 424, 425, 426, 428, 429, 431, 451, 500, 501, 502, 503, 504, 505, 506, 507, 508, 510, 511]
+
+
+def extract_number(string):
+    # Use a regex to find any sequence of digits in the string
+    match = re.search(r'\d+', string)
+    if match:
+        # Convert the matched string to an integer
+        try:
+            return int(match.group())
+        except ValueError:
+            # If the value is not a valid integer, return None
+            return 404
+    else:
+        # If no match was found, return None
+        return 404
+
+def check_code(number):
+    return number in response_codes
+
+
+@client.event
+async def on_ready():
+    print(f'Logged in...')
+
+
+@client.event
+async def on_message(message):
+    # If the message is from the bot, ignore it
+    if message.author == client.user:
+        return
+
+    # If the message starts with "http."
+    if message.content.startswith('http.'):
+            # Attempt to extract the response code from the message
+            message_code = extract_number(message.content)
+            if check_code(message_code):
+                await message.channel.send(f'https://http.cat/{message_code}.jpg')
+            else:
+                await message.channel.send(f'https://http.cat/404.jpg')
+
+client.run(os.getenv('TOKEN'))
